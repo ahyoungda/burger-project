e9ad87f0f4d5bdafb5fdc25c0cb4377c2e0c3b20
euan.may24@gmail.com <euan.may24@gmail.com>
2024-06-25 23:35:08 +0900

    [chore] DB 계정 생성 및 권한 부여
    User: burgerland
    DB: burgerdb

--- DB_BURGER.sql
+++ DB_BURGER.sql
@@ -1,3 +1,19 @@
+create user 'burgerland'@'%' identified by 'burgerland';
+
+use mysql
+select *
+from user;
+
+create database burgerdb;
+
+show databases;
+
+grant all privileges on burgerdb.* to 'burgerland'@'%';
+
+show grants for 'burgerland'@'%';
+
+use burgerdb;
+
 DROP TABLE `메뉴`;
 
 CREATE TABLE `메뉴`
@@ -39,7 +55,5 @@ CREATE TABLE `고객`
     PRIMARY KEY ( `customer_code` )
 ) COMMENT = '고객';
 
-ALTER TABLE `고객`
-    ADD CONSTRAINT `고객_PK` PRIMARY KEY ( `customer_code` );
 
 
---
+++ DB_USER_PRIV.sql
@@ -0,0 +1,17 @@
+create
+user 'burgerland'@'%' identified by 'burgerland';
+
+use
+mysql
+select *
+from user;
+
+create database burgerdb;
+
+show databases;
+
+grant all privileges on burgerdb.* to 'burgerland'@'%';
+
+show grants for 'burgerland'@'%';
+
+use burgerdb
