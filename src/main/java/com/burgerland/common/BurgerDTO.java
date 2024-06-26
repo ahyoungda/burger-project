378676fdc4b7f142b0907eee9c1ec86b97fe27e1
MANTISKYU <asdaron44@gmail.com>
2024-06-26 11:20:16 +0900

    [FEAT] DTO 구현

--- src/main/java/com/burgerland/common/BurgerDTO.java
+++ src/main/java/com/burgerland/common/BurgerDTO.java
@@ -1,4 +1,24 @@
 package com.burgerland.common;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class BurgerDTO {
+
+    private int customerCode;
+    private String customerName;
+    private String regDate;
+    private String customerGrade;
+    private int orderCode;
+    private int menuCode;
+    private String orderDate;
+    private String menuName;
+    private int menuPrice;
+    private String category;
+    
 }
