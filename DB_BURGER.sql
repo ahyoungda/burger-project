c289320f6ea29036ceb467433a7ae912effe9d55
euuuuuuan <euan.may24@gmail.com>
2024-06-26 09:42:26 +0900

    [chore] DB내 유저로그인 삭제

--- DB_BURGER.sql
+++ DB_BURGER.sql
@@ -1,19 +1,3 @@
-create user 'burgerland'@'%' identified by 'burgerland';
-
-use mysql
-select *
-from user;
-
-create database burgerdb;
-
-show databases;
-
-grant all privileges on burgerdb.* to 'burgerland'@'%';
-
-show grants for 'burgerland'@'%';
-
-use burgerdb;
-
 DROP TABLE `메뉴`;
 
 CREATE TABLE `메뉴`
