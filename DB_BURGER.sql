09af61db50060fc35f6cef8060ca5a594263c7cc
MANTISKYU <asdaron44@gmail.com>
2024-06-26 09:49:40 +0900

    [chore] DB 작성
    한글로 작성된 몇몇 DB테이블명을 영문으로 바꿈

--- DB_BURGER.sql
+++ DB_BURGER.sql
@@ -1,43 +1,45 @@
-DROP TABLE `메뉴`;
+DROP TABLE `menu`;
 
-CREATE TABLE `메뉴`
+CREATE TABLE `menu`
 (
     `menu_code`    INTEGER(255) NOT NULL COMMENT '메뉴코드',
     `menu_name`    VARCHAR(255) NOT NULL COMMENT '메뉴명',
     `menu_price`    INTEGER(255) NOT NULL COMMENT '가격',
     `category`    VARCHAR(255) NOT NULL COMMENT '카테고리',
     PRIMARY KEY ( `menu_code` )
-) COMMENT = '메뉴';
+) COMMENT = 'menu';
 
-ALTER TABLE `메뉴`
-    ADD CONSTRAINT `메뉴_PK` PRIMARY KEY ( `menu_code` );
-
-
-DROP TABLE `주문`;
-
-CREATE TABLE `주문`
-(
-    `order_code`    INTEGER(255) NOT NULL COMMENT '주문코드',
-    `customer_code`    INTEGER(255) NOT NULL COMMENT '고객코드',
-    `menu_code`    INTEGER(255) NOT NULL COMMENT '메뉴코드',
-    `order_date`    VARCHAR(255) NOT NULL COMMENT '주문일',
-    PRIMARY KEY ( `order_code` )
-) COMMENT = '주문';
-
-ALTER TABLE `주문`
-    ADD CONSTRAINT `주문_PK` PRIMARY KEY ( `order_code` );
+ALTER TABLE `menu`
+    ADD CONSTRAINT `menu_PK` PRIMARY KEY ( `menu_code` );
 
 
-DROP TABLE `고객`;
+DROP TABLE `customer`;
 
-CREATE TABLE `고객`
+CREATE TABLE `customer`
 (
     `customer_code`    INTEGER(255) NOT NULL COMMENT '고객코드',
     `customer_name`    VARCHAR(255) NOT NULL COMMENT '고객이름',
     `reg_date`    VARCHAR(255) NOT NULL COMMENT '가입일',
     `customer_grade`    VARCHAR(255) NOT NULL COMMENT '고객등급',
     PRIMARY KEY ( `customer_code` )
-) COMMENT = '고객';
+) COMMENT = 'customer';
 
+ALTER TABLE `customer`
+    ADD CONSTRAINT `customer_PK` PRIMARY KEY ( `customer_code` );
+
+
+DROP TABLE `order`;
+
+CREATE TABLE `order`
+(
+    `order_code`    INTEGER(255) NOT NULL COMMENT '주문코드',
+    `customer_code`    INTEGER(255) NOT NULL COMMENT '고객코드',
+    `menu_code`    INTEGER(255) NOT NULL COMMENT '메뉴코드',
+    `order_date`    VARCHAR(255) NOT NULL COMMENT '주문일',
+    PRIMARY KEY ( `order_code` )
+) COMMENT = 'order';
+
+ALTER TABLE `order`
+    ADD CONSTRAINT `order_PK` PRIMARY KEY ( `order_code` );
 
 
