695e0149ac0d39a145e86be360e769811d7e349b
euuuuuuan <euan.may24@gmail.com>
2024-06-28 12:08:25 +0900

    [temp] 임시저장

--- src/main/java/com/burgerland/controller/OrderController.java
+++ src/main/java/com/burgerland/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.burgerland.controller;
 
+import com.burgerland.common.OrderDTO;
 import com.burgerland.service.OrderService;
 import com.burgerland.view.OrderPrintResult;
 
@@ -21,8 +22,17 @@ public class OrderController {
     // 4. 최종 출력은
     // {주문 번호}, {메뉴 이름}, {메뉴 가격}, {메뉴별 주문 개수}, {주문일}
     // 내용이 담긴 영수증이 출력된다.
-    public void registOrder(String menuName) {
+    public void registOrder(Map<String, String> parameter) {
         String menuName = parameter.get("menuName");
+        int custContact = Integer.parseInt(parameter.get("custContact"));
+
+        OrderDTO order = new OrderDTO();
+        order.setOrderName(menuName);
+        order.setOrderContact(custContact);
+
+        if (orderService.registOrder(order)) {
+            orderPrintResult.printSuccessMessage("order")
+        }
 
     }
 }
